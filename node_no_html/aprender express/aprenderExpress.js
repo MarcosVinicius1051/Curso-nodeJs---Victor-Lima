256833a945877956f4845dd472baeef409f4d9c2
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2025-04-17 11:34:36 -0300

    parametros

--- node_no_html/aprender express/aprenderExpress.js
+++ node_no_html/aprender express/aprenderExpress.js
@@ -1,19 +1,23 @@
-import express from "express"
+import express from "express";
 
 let app = express();
 
-app.get("/",(req,res)=>{
-res.send("bem vindo ao começo")
-})
-app.get("/sobre",(req,res)=>{
-res.send("esse é um projeto simples de aprendizado")
-})
-app.get("/conta",(req,res)=>{
-res.send("nome: Marcos vinicius")
-})
-
-app.listen(4041,()=>{
-    console.log("servidor está aberto")
+app.get("/", (req, res) => {
+  res.send("bem vindo ao começo");
+});
+app.get("/sobre", (req, res) => {
+  res.send("esse é um projeto simples de aprendizado");
+});
+app.get("/conta", (req, res) => {
+  res.send("nome: Marcos vinicius");
+});
+app.get("/:nome", (req, res) => {
+    res.send(`bem vindo ${req.params.nome}`)
+    /* aqui estamo utilizado parametro, os quais são criado usando ":nomeParametro", eles funcionam colocando no link de pesquisa do site, além disso eles podem ser "resquisitado" com o comando   "req.params.nomeParametro" */
+});
+app.listen(4041, () => {
+  console.log("servidor está aberto");
 });
 
-// o comanod app.listen() deve sempre estar no final do código. 
+// o comanod app.listen() deve sempre estar no final do código.
+// node aprenderExpress.js
