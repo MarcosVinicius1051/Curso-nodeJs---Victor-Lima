1bd3b822b2a185865e646b6b54e949510f747f5b
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2025-04-20 14:35:45 -0300

    comandos basicos sql

---
+++ sql/teste.sql
@@ -0,0 +1,9 @@
+/* aqui apenas será o local em que irei digitar alguns comandos. nada disso sera realmente usado no sql*/
+
+INSERT INTO usuarios(nome,email,idade)VALUES(
+    "galego",
+    "galego@gmail.com",
+    4
+);
+
+SELECT * FROM usuarios WHERE idade>20;
