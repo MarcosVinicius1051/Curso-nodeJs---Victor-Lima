1cab0644297846d08b11197a738a2e1f69768392
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2025-04-20 14:36:28 -0300

    sequelize install

--- node_no_html/aprender express/aprenderExpress.js
+++ node_no_html/aprender express/aprenderExpress.js
@@ -1,6 +1,8 @@
 import express from "express";
+import path from "path";
 
 let app = express();
+let __dirname = path.resolve(); /* Este sistema garante que o express pegue os arquivos corretos da nossa pasta.  */
 
 app.get("/", (req, res) => {
   res.send("bem vindo ao começo");
@@ -11,10 +13,13 @@ app.get("/sobre", (req, res) => {
 app.get("/conta", (req, res) => {
   res.send("nome: Marcos vinicius");
 });
-app.get("/:nome", (req, res) => {
+app.get("/param/:nome", (req, res) => {
     res.send(`bem vindo ${req.params.nome}`)
     /* aqui estamo utilizado parametro, os quais são criado usando ":nomeParametro", eles funcionam colocando no link de pesquisa do site, além disso eles podem ser "resquisitado" com o comando   "req.params.nomeParametro" */
 });
+app.get("/html",(req,res)=>{
+res.sendFile(__dirname + "/paginasHtml/exemplo1.html") /*tomar cuidado, já que esse comando "sendFile" pode acarretar em não pegar o local em que o arquivo está. É necessario resolver esse problema */
+})
 app.listen(4041, () => {
   console.log("servidor está aberto");
 });
